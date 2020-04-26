aed5875496675b4fa4815d3e0ca9d9d0cf500cab
code-rgb <62891774+code-rgb@users.noreply.github.com>
2020-04-26 17:44:12 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -26,12 +26,10 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("You are running OpenUserBot [v7.7.7]")
+LOGS.info("You are running OUB-X [v69.420]")
 
 LOGS.info(
-    "😘 Congratulations, your userbot is now running !! Test it by typing .ping in any chat 😘."
-    "Once died 😵, but now ON again 😉, A BIG thanks to https://github.com/Devp73 Dev Patel who have helped make this userbot ON AGAIN with his HUGE FIX!!"
-    "for command list type .help & If you need assistance, head to https://t.me/PPE_Support")
+    "🔴🔵🔴🔵🔴 OUB-X is Alive 🔴🔵🔴🔵🔴", "for command list type .help & If you need assistance, head to https://t.me/PPE_Support")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
